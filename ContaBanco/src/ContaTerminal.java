c6a2842f4461af3cefb1235852f5f461ce5604c5
LuizzDev <lhso1991@gmail.com>
2024-08-26 13:55:18 -0300

    a

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -4,32 +4,46 @@ import java.util.Scanner;
 public class ContaTerminal {
     public static void main(String[] args)
      {
+        //Criando objeto scanner
+        
         Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
 
-        /** 
-         * Coletando dados do cliente.
-         */
+         
+        //Coletando dados do cliente.
+         
         System.out.println("Digite seu nome: ");
+
         String nome = scanner.next();
+
         
         System.out.println("Digite seu sobreNome: ");
+
         String sobreNome = scanner.next();
 
+
         System.out.println("Digite número da conta: ");
+
         int numeroConta = scanner.nextInt();
 
+
         System.out.println("Digite número da Agência: ");
+
         String numeroAgencia = scanner.next();
 
+
         System.out.println("Informe valor de deposito: ");
+
         double valorDeposito = scanner.nextDouble();
 
-        /**
-         * Informações do cliente.
-         */
+        
+        //Informações do cliente.
+         
         System.out.println("Olá " + nome +" "+ sobreNome + ", obrigado por criar uma conta em nosso banco");
+
         System.out.println("Sua agência é " + numeroAgencia);
+
         System.out.println("Número da Conta " + numeroConta);
+
         System.out.println("Saldo de R$" + valorDeposito + " já está disponível para saque");
 
     }
