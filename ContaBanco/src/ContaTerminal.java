8b712e05fe545e36675b08c28827765861a43a98
LuizzDev <lhso1991@gmail.com>
2024-08-26 13:21:36 -0300

    comentário

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -6,6 +6,9 @@ public class ContaTerminal {
      {
         Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
 
+        /** 
+         * Coletando dados do cliente.
+         */
         System.out.println("Digite seu nome: ");
         String nome = scanner.next();
         
@@ -21,7 +24,9 @@ public class ContaTerminal {
         System.out.println("Informe valor de deposito: ");
         double valorDeposito = scanner.nextDouble();
 
-
+        /**
+         * Informações do cliente.
+         */
         System.out.println("Olá " + nome +" "+ sobreNome + ", obrigado por criar uma conta em nosso banco");
         System.out.println("Sua agência é " + numeroAgencia);
         System.out.println("Número da Conta " + numeroConta);
